8d4bbadf541d5cbfdce25e80eabdd11746220d88
emilyhmarkowitz <emilymarkowitz@gmail.com>
2020-10-07 17:06:06 -0400

    General update to files

--- DownloadStuff.R
+++ DownloadStuff.R
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 #Vinettes
 
@@ -9,23 +7,5 @@ library("googledrive")
 # https://docs.google.com/spreadsheets/d/1NkG331BZW_Q9PuzdhEgW5rB6vkLe0K_Dd1K3iFrAFNw/edit?usp=sharing
 drive_deauth()
 drive_auth()
-drive_download("Welcome/About", type = ".doc", overwrite = TRUE, path = "./Welcome_About")
+drive_download("Welcome/About", type = ".txt", overwrite = TRUE, path = "./Welcome_About")
 drive_download("Member Profiles", type = "csv", overwrite = TRUE, path = "./Member_Profiles")
-
-
-=======
-
-
-#Vinettes
-
-library("googledrive")
-
-# https://googledrive.tidyverse.org/
-# https://docs.google.com/spreadsheets/d/1NkG331BZW_Q9PuzdhEgW5rB6vkLe0K_Dd1K3iFrAFNw/edit?usp=sharing
-drive_deauth()
-drive_auth()
-drive_download("Welcome/About", type = ".doc", overwrite = TRUE, path = "./Welcome_About")
-drive_download("Member Profiles", type = "csv", overwrite = TRUE, path = "./Member_Profiles")
-
-
->>>>>>> 10f9fbddb4b44276d4584ff5b694703b93dd0591
